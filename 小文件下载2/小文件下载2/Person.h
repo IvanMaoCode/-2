4018b4077a7311bb1f03c225e00559bfa951b0ee
XMG <kluthmao@163.com>
2019-08-18 10:25:34 +0800

    创建了Person类

---
+++ 小文件下载2/小文件下载2/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  小文件下载2
+//
+//  Created by Kluth on 2019/8/18.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 小文件下载2/小文件下载2/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  小文件下载2
+//
+//  Created by Kluth on 2019/8/18.
+//  Copyright © 2019 Kluth. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
